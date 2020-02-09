a4669ee8ee8c221dc7bcb87d5f1bb09342d857d0
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-02-10 02:10:52 +0530

    Update login.js

--- client/src/Containers/login/login.js
+++ client/src/Containers/login/login.js
@@ -9,63 +9,68 @@ import CardContent from '@material-ui/core/CardContent';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
-import { withStyles } from '@material-ui/core/styles';  
+import { withStyles, createMuiTheme} from '@material-ui/core/styles';  
 import Container from '@material-ui/core/Container';
+ 
+const theme=createMuiTheme();
+
+const styles={
+    root:{
+        paddingLeft:'0px',
+        paddingRight:'0px'
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        flexDirection: 'column',
+        alignItems: 'center'
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        // marginTop: theme.spacing(1),
+      },
+      cardContent:{
+          margin: '0 auto'
+      }
+  };
 
 class Login extends Component{
     render(){
-
-        const {classes} = props;
-        const styles={
-            root: {
-              marginTop: theme.spacing(8),
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }
-            // avatar: {
-            //   margin: theme.spacing(1),
-            //   backgroundColor: theme.palette.secondary.main,
-            // },
-            // form: {
-            //   width: '100%', // Fix IE 11 issue.
-            //   marginTop: theme.spacing(1),
-            // },
-            // submit: {
-            //   margin: theme.spacing(3, 0, 2),
-            // },
-          };
+        const {classes} = this.props;
         return(
-                <Container maxWidth="xs" >
+                <Container maxWidth="xs" classes={{ root:classes.root}} >
                     <Card variant="outlined" className={classes.paper}>
-                        <CardContent>
-                        <Avatar>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography variant="h5">
-                            Sign in
-                        </Typography>
-                        <Typography variant="subtitle1" >
-                            Use your Account
-                        </Typography>
+                        <CardContent className={classes.cardContent} >
+                            <Avatar className={classes.avatar}  >
+                                <LockOutlinedIcon />
+                            </Avatar>
+                            <Typography variant="h5">
+                                Sign in
+                            </Typography>
+                            <Typography variant="subtitle1" >
+                                Use your Account
+                            </Typography>
                         </CardContent>
                         <CardActions>
-                        <form>
-                            <TextField 
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"  
-                                label="Email"
-                                type="email"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                                // error
-                                // helperText="Couldn't find your account"
-                            />
-                            <Grid container>
+                            <form className={classes.form} >
+                                <TextField 
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"  
+                                    label="Email"
+                                    type="email"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                    // error
+                                    // helperText="Couldn't find your account"
+                                />
+                                <Grid container>
                                 <Grid item xs>
                                     <Link href="#" variant="body2">
                                         Create Account
@@ -81,16 +86,12 @@ class Login extends Component{
                                     </Button>
                                 </Grid>
                             </Grid>
-                        </form>
+                            </form>
                         </CardActions>
                     </Card>
-                </Container>
+                </Container>    
         )
     }
 }
 
-Login.propTypes = {
-    classes: PropTypes.object.isRequired
-  };
-
-export default Login(styles)(Login);
+export default  withStyles(styles)(Login);
