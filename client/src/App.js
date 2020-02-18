e5292d2a39e23ff470cbe62f84b710f6b362cda3
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-02-18 21:39:22 +0530

    Update redirect routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
-import Login from './Containers/login/login';
+import {Route , Redirect} from 'react-router-dom';
+import LoginEmail from './Containers/login/login-email';
+import LoginPassword from './Containers/login/login-password';
 
 class App extends Component{
   render(){
     return(
       <div>
-        <Route path="/" component={Login} />
+        <Route path="/signin" exact component={LoginEmail} />
+        <Redirect from="/" to="/signin" />
+        <Route path="/signin/pwd" exact component={LoginPassword} />
       </div>
     )
   }
