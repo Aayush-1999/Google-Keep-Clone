cbb76b347b94457fb89f02e2fdc6025df1f7e880
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-02-19 20:06:36 +0530

    Add route path for signup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route , Redirect} from 'react-router-dom';
 import LoginEmail from './Containers/login/login-email';
 import LoginPassword from './Containers/login/login-password';
+import SignUp from './Containers/signup/signup';
 
 class App extends Component{
   render(){
@@ -10,6 +11,7 @@ class App extends Component{
         <Route path="/signin" exact component={LoginEmail} />
         <Redirect from="/" to="/signin" />
         <Route path="/signin/pwd" exact component={LoginPassword} />
+        <Route path="/signup" exact component={SignUp} />
       </div>
     )
   }
