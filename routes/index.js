b5f7f056c5c13112080319ecdfdaeed98375b4d1
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-03-01 16:39:30 +0530

    complete login/checkEmail route and add login/checkPwd route

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,56 @@
 const express      = require("express"),
       router       = express.Router(),
       passport     = require("passport"),
-      bcrypt       = require("bcrypt"),
       User         = require("../models/user");
 
-
 //REGISTER LOGIC ROUTE
-router.post("/register",async function(req,res){
-    try{
-        let saltRounds=10,hashcode;    
-        hashcode = await bcrypt.hash(req.body.password, saltRounds)
-        let user = await User.create({
-            firstName:req.body.firstname,
-            lastName:req.body.lastname,
-            displayName:req.body.firstname + " " + req.body.lastname,
-            email:req.body.email,
-            password:hashcode,
-            image:"https://res.cloudinary.com/image-storage/image/upload/v1572009434/blank-avatar_opbhgx.png"
-        }); 
-        req.logIn(user,function(err){  
-            res.redirect("/");
-        });                   
-    }
-    catch(err){
-        req.flash("error","This Email is already registered");
-        res.redirect("/register");
-    }    
-});
+// router.post("/register",async function(req,res){
+//     try{
+//         let saltRounds=10,hashcode;    
+//         hashcode = await bcrypt.hash(req.body.password, saltRounds)
+//         let user = await User.create({
+//             firstName:req.body.firstname,
+//             lastName:req.body.lastname,
+//             displayName:req.body.firstname + " " + req.body.lastname,
+//             email:req.body.email,
+//             password:hashcode,
+//             image:"https://res.cloudinary.com/image-storage/image/upload/v1572009434/blank-avatar_opbhgx.png"
+//         }); 
+//         req.logIn(user,function(err){  
+//             res.redirect("/");
+//         });                   
+//     }
+//     catch(err){
+//         req.flash("error","This Email is already registered");
+//         res.redirect("/register");
+//     }    
+// });
 
 
 //LOCAL LOGIN LOGIC ROUTE
-router.post("/login",passport.authenticate("local",{ 
-        failureRedirect:"/login",
-        failureFlash:"Invalid email or password"
-    }),(req,res)=>{
-        res.redirect("/");
-});
+// router.post("/login",passport.authenticate("local",{ 
+//         failureRedirect:"/login",
+//         failureFlash:"Invalid email or password"
+//     }),(req,res)=>{
+//         res.redirect("/");
+// });
  
-router.post("/login/checkEmail",(req,res)=>{
-    let user = User.find({email:email})
+router.post("/login/checkEmail",async(req,res)=>{
+    try{
+        let user = await User.find({email:req.body.email})
+        if(user!=null) res.status(200).send("user email found")
+        else res.status(404).send("email not found");
+    }
+    catch(err){
+        res.status(204).send(err);
+    }
+})
+
+//add password matching code
+router.post("/login/checkPwd",async (req.res)=>{
+    try{
+
+    }
 })
 
 //LOGOUT ROUTE
