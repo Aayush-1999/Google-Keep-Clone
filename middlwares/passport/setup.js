7cf5e309eaf5e3195901a02138a799ee236c3b38
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-03-03 21:36:31 +0530

    Remove mongoose dependance

--- middlwares/passport/setup.js
+++ middlwares/passport/setup.js
@@ -2,7 +2,6 @@ const passport              = require("passport"),
       local                 = require("./localStrategy");
       User                  = require("../../models/user"),
       expressSession        = require("express-session"),
-      mongoose              = require("mongoose");
 
 module.exports = app =>{
     app.use(expressSession({
