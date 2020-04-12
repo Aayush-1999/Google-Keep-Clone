1074cff2f6b6cddb6cddd2cdd02abad8846942b6
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-04-13 03:41:33 +0530

    Add jwt token in register and login route

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,33 @@ const express      = require("express"),
       router       = express.Router(),
       bcrypt       = require("bcryptjs"),
       passport     = require("passport"),
+      jwt          = require("jsonwebtoken"),
       User         = require("../models/user");
 
 //REGISTER ROUTE
-router.post("/register",async function(req,res){
-    try{
+router.post("/register",async(req,res)=>{
+    try{   
+        let user = await User.findOne({email:req.body.email})
+        if(user) return res.status(200).json({msg:"User already exists"})
         let salt = bcrypt.genSaltSync(10);
         let hash = bcrypt.hashSync(req.body.password, salt);
-        let user = await User.create({
+        user = await User.create({
             firstName:req.body.firstname,
             lastName:req.body.lastname,
-            displayName:req.body.firstname + " " + req.body.lastname,
             email:req.body.email,
             password:hash,
             image:"https://res.cloudinary.com/image-storage/image/upload/v1572009434/blank-avatar_opbhgx.png"
         });
-        res.status(200).send(user);
+        console.log(user)
+        const token=jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET_KEY,
+            { expiresIn:3600 }
+        )
+        res.status(200).json({user,token});
     }
     catch(err){
-        res.status(400).send("registeration unsuccessful");
+        res.status(400).json({msg:"registeration unsuccessful"});
     }    
 });
 
@@ -28,8 +36,8 @@ router.post("/register",async function(req,res){
 router.post("/login/checkEmail",async(req,res)=>{
     try{
         let user = await User.findOne({email:req.body.email})
-        if(user!=null) res.status(200).send("user email found")
-        else res.status(404).send("email not found");
+        if(user!=null) res.status(200).json({msg:"user email found"})
+        else res.status(404).json({msg:"email not found"});
     }
     catch(err){
         res.status(204).send(err);
@@ -37,9 +45,23 @@ router.post("/login/checkEmail",async(req,res)=>{
 })
 
 //LOGIN ROUTE- VERIFY PASSWORD
-router.post("/login/checkPwd",passport.authenticate("local"),(req, res) => {
-    let userInfo = req.user;
-    res.send(userInfo);
+router.post("/login/checkPwd",async(req, res) => {
+    try{
+        const user = await User.findOne({email:req.body.email})
+        bcrypt.compare(req.body.password,user.password)
+            .then(isMatch=>{
+                if(!isMatch) return res.status(400).json({msg:"Invalid Password"})
+                const token=jwt.sign(
+                    {id:user._id},
+                    process.env.JWT_SECRET_KEY,
+                    { expiresIn:3600 }
+                ) 
+                res.status(200).json({user,token});
+            })
+    }
+    catch(err){
+        res.status(400).json({msg:"Something went wrong"})
+    }
 });
 
 //LOGOUT ROUTE
