97b385372d1795d83ffe964740f1373e177db3c5
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-04-22 04:10:38 +0530

    Add required things for configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
+import authReducer from './store/reducers/auth';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <CssBaseline />
-        <App />
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer=combineReducers({
+    auth:authReducer
+})
+
+const store=createStore(rootReducer,composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+const app=(
+    <Provider store={store} >
+       <BrowserRouter>
+            <CssBaseline />
+            <App />
     </BrowserRouter>
-    , document.getElementById('root'));
+    </Provider>
+);
+
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
