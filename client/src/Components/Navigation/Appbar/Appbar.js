307971cffe59705e0a532c0ce3ad0d6f4ca8797f
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-06-15 02:28:04 +0530

    Update Appbar.js

--- client/src/Components/Navigation/Appbar/Appbar.js
+++ client/src/Components/Navigation/Appbar/Appbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -55,19 +54,21 @@ function Header(props) {
           <Typography className={classes.title} variant="h6" noWrap>
             Keep
           </Typography>
-          {/* <div className={classes.search}> */}
-            {/* <div className={classes.searchIcon}> */}
-              {/* <SearchIcon /> */}
-            {/* </div> */}
+          <div className={classes.search}>
+            <IconButton className={classes.searchIcon}>
+              <SearchIcon />
+            </IconButton>
             <InputBase
+              fullWidth
               placeholder="Search"
               classes={{
-                root: clsx(classes.inputRoot,classes.search),
+                root: classes.inputRoot,
                 input: classes.inputInput,
+                focused: classes.inputFocused
               }}
               inputProps={{ 'aria-label': 'search' }}
             />
-          {/* </div> */}
+          </div>
           <div className={classes.grow} />
           <div className={classes.accountOptions}>
             <IconButton
