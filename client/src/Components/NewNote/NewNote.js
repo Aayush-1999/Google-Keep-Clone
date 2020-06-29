e2f90728c4fa278ccca105a2051de097f23f4527
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-06-30 00:45:05 +0530

    Create basic outlet for new note

---
+++ client/src/Components/NewNote/NewNote.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styles from './NewNote.styles';
+
+const useStyles = makeStyles(styles);
+
+function NewNote(props){
+    const classes = useStyles();
+    const [isShow, setIsShow] = React.useState(false);
+
+    const handleContentClick = () => {
+        setIsShow(true);
+    }
+
+    return(
+        <div className = {classes.cardContainer}>
+            <Card classes = {{root : classes.card }} >
+                <CardContent classes={{root : classes.CardContent}} onClick={handleContentClick} >
+                    <TextField id="standard-basic" placeholder = { isShow ? "Title" : "Take a note..." } />
+                </CardContent>
+                {isShow &&
+                <div>
+                    <TextField id="standard-basic" placeholder="Take a note..." />
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </div>}
+            </Card>
+        </div>
+    )
+}
+
+export default NewNote;
