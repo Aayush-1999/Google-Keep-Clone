8a1d0b592d711e945231e04352be4d5fbee3bfff
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-07-04 23:59:16 +0530

    Fix padding of card

--- client/src/Components/NewNote/NewNote.styles.js
+++ client/src/Components/NewNote/NewNote.styles.js
@@ -6,6 +6,7 @@ export default theme => ({
         [theme.breakpoints.up('md')]: {
             width: '600px'  
         },
+        // boxShadow: 'inset 1px 1px 0 rgba(0,0,0,.1), inset 0 -1px 0 rgba(0,0,0,.07)',
         boxShadow: '0 1px 2px 0 rgba(60,64,67,0.302), 0 2px 6px 2px rgba(60,64,67,0.149)',
         borderRadius: '6px'
     },
@@ -29,6 +30,6 @@ export default theme => ({
         color: '#80868b'
     },
     textContainer: {
-        padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+        padding: `9px ${theme.spacing(2)}px ${theme.spacing(1)}px ${theme.spacing(2)}px`,
     }
 })
