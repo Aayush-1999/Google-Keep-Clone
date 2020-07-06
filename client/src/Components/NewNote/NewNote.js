ac9ddc90c37f185cd1ac41260f3d9080f31eb69c
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-07-07 02:03:57 +0530

    New Note basic ui structure complete

--- client/src/Components/NewNote/NewNote.js
+++ client/src/Components/NewNote/NewNote.js
@@ -5,7 +5,10 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { CheckBoxOutlined, ImageOutlined, AddAlertOutlined, ColorLensOutlined, ArchiveOutlined, MoreVertOutlined, UndoOutlined, RedoOutlined } from '@material-ui/icons';
 import styles from './NewNote.styles';
+import clsx from 'clsx';
 
 const useStyles = makeStyles(styles);
 
@@ -22,16 +25,45 @@ function NewNote(props){
             <Card classes = {{root : classes.card }} >
                 <CardContent classes={{root : classes.cardContent}} onClick={handleContentClick} >
                     <TextField placeholder = { isShow ? "Title" : "Take a note..." } 
-                     InputProps = {{ disableUnderline : true, classes: {input: classes.inputPlaceholder} }} 
-                    classes={{root: classes.textContainer}} fullWidth autoFocus size='small'/>
+                        InputProps = {{ disableUnderline : true, classes: {input: classes.inputPlaceholder} }} 
+                        classes={{root: clsx(classes.textContainer,{ 
+                            [classes.textContainerWidthxL]: isShow })}} autoFocus size='small'
+                        multiline 
+                    />
+                    { !isShow &&
+                    <span>
+                        <IconButton>
+                            <CheckBoxOutlined  />
+                        </IconButton>
+                        <IconButton>
+                            <ImageOutlined />
+                        </IconButton>
+                    </span>
+                    }
+                    { isShow && 
+                    <IconButton>
+                        <span class="material-icons-outlined">
+                            push_pin
+                        </span>    
+                    </IconButton>
+                    }
                     { isShow &&
-                        <TextField placeholder = "Take a note..."  InputProps = {{disableUnderline : true}} 
-                        classes={{root: classes.textContainer, classes: {input: classes.inputPlaceholder} }} fullWidth />
+                    <TextField placeholder = "Take a note..."  InputProps = {{disableUnderline : true}} 
+                        classes={{root: classes.textContainer, classes: {input: classes.inputPlaceholder} }} fullWidth 
+                        multiline autoFocus
+                    />
                     }
                 </CardContent>
                 {isShow &&
                 <CardActions>
-                    <Button size="small">Learn More</Button>
+                    <IconButton><AddAlertOutlined /></IconButton>
+                    <IconButton><ColorLensOutlined /></IconButton>
+                    <IconButton><ImageOutlined /></IconButton>
+                    <IconButton><ArchiveOutlined /></IconButton>
+                    <IconButton><MoreVertOutlined /></IconButton>
+                    <IconButton><UndoOutlined /></IconButton>
+                    <IconButton><RedoOutlined /></IconButton>
+                    <Button size="small">Close</Button>
                 </CardActions>
                 }
             </Card>
