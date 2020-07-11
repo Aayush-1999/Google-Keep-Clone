3f6d09275ed1bd3897250e08e98505446d8e454f
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-07-11 23:39:42 +0530

    Remove resetPassword attributes from user model

--- models/user.js
+++ models/user.js
@@ -1,27 +1,25 @@
 const mongoose   = require("mongoose");
 
 const UserSchema=new mongoose.Schema({
-    firstName:{
-        type:String,
-        default:null
+    firstName: {
+        type: String,
+        default: null
     },
-    lastName:{
-        type:String,
-        default:null
+    lastName: {
+        type: String,
+        default: null
     },
-    email:{
-        type:String,
-        unique:true,
-        required:true
+    email: {
+        type: String,
+        unique: true,
+        required: true
         },
-    password:String,
-    image:String,
-    imageId:{
-        type:String,
-        default:null
-    },
-    resetPasswordToken:String,
-    resetPasswordExpires:Date    
+    password: String,
+    image: String,
+    imageId: {
+        type: String,
+        default: null
+    }
 })
 
 module.exports=mongoose.model("User",UserSchema);
