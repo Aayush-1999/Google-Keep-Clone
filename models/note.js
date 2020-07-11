a4e30bb9b8447625d06ba481017b15f8f8344fea
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-07-11 23:40:06 +0530

    Update note schema

--- models/note.js
+++ models/note.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 
 const noteSchema = mongoose.Schema({
-    id:String,
-    title:String,
-    text:String,
-    label:String,
-    image:String,
-    imageId:String,
-    color:{
-        type:String,
-        default:"white"
-    },
-    pinned:{
-        type:Boolean,
+    id: String,
+    title: String,
+    text: String,
+    label: [ String ],
+    image: String,
+    imageId: String,
+    checkBox : {
+        type: Boolean,
         default : false
     },
-    archive:{
-        type:Boolean,
-        default : false
+    checkPoint: [ String ],
+    color: {
+        type: String,
+        default: "white"
     },
-    author:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+    pinned: {
+        type: Boolean,
+        default :  false
+    },
+    archive: {
+        type: Boolean,
+        default :  false
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
 })
 
