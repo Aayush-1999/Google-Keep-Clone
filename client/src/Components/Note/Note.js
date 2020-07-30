cc6c3bcd91182f5a552a896d9b94280cd4794b9e
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-07-30 23:55:08 +0530

    Add basic structure for Note.js

---
+++ client/src/Components/Note/Note.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import { CheckBoxOutlined, ImageOutlined, AddAlertOutlined, ColorLensOutlined, ArchiveOutlined, MoreVertOutlined, UndoOutlined, RedoOutlined } from '@material-ui/icons';
+import clsx from 'clsx';
+import styles from './Note.styles';
+
+const useStyles = makeStyles(styles);
+
+function Note(props){
+    const classes = useStyles();
+
+    return(
+        <div>
+            <Card>
+
+            </Card>
+        </div>
+    )
+}
+
+export default Note;
